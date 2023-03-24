8d79f639572a67dda860c83bcab727c8d09574b2
FridIsar <isarfridriksson@gmail.com>
2023-03-24 14:13:27 +0100

    itoa cannot be tested

--- ft_itoa.c
+++ ft_itoa.c
@@ -3,13 +3,58 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ifridrik <ifridrik@student.42.fr>          +#+  +:+       +#+        */
+/*   By: openculturalcenter <openculturalcenter@    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 12:54:44 by ifridrik          #+#    #+#             */
-/*   Updated: 2023/03/17 12:54:45 by ifridrik         ###   ########.fr       */
+/*   Updated: 2023/03/24 13:49:17 by opencultura      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_itoa(int n);
+int	ft_countdigits(int n)
+{
+	int				i;
+	unsigned int	un;
+
+	if (n < 0)
+		un = -n;
+	else
+		un = n;
+	i = 1;
+	while (un > 9)
+	{
+		un /= 10;
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_itoa(int n)
+{
+	unsigned int	un;
+	char			*str;
+	int				len;
+
+	len = ft_countdigits(n);
+	if (n < 0)
+	{
+		un = -n;
+		len++;
+	}
+	else
+		un = n;
+	str = malloc(len * sizeof (char));
+	if (!str)
+		return (NULL);
+	while (un > 9)
+	{
+		str[len - 1] = (un % 10) + '0';
+		len--;
+		un /= 10;
+	}
+	str[len - 1] = (un % 10) + '0';
+	if (n < 0)
+		str[0] = '-';
+	return (str);
+}
--- libft.h
+++ libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ifridrik <ifridrik@student.42.fr>          +#+  +:+       +#+        */
+/*   By: openculturalcenter <openculturalcenter@    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 14:05:24 by ifridrik          #+#    #+#             */
-/*   Updated: 2023/03/17 12:57:30 by ifridrik         ###   ########.fr       */
+/*   Updated: 2023/03/24 14:01:16 by opencultura      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,11 +44,11 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
-char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+char	*ft_itoa(int n);
 #endif // LIBFT_H
