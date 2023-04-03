3c7a6980514be523dfac43ec94435188e9fab247
Isar Fridriksson <ifridrik@cbr15s4.42barcelona.com>
2023-04-03 16:13:55 +0200

    rm comment

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,16 +6,11 @@
 /*   By: ifridrik <ifridrik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 17:13:22 by ifridrik          #+#    #+#             */
-/*   Updated: 2023/03/10 09:06:41 by ifridrik         ###   ########.fr       */
+/*   Updated: 2023/04/03 16:13:39 by ifridrik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-/*
-** is it too much to multiply line 28 by sizeof char?
-** or is it already counted in bytes?
-*/
 
 void	*ft_calloc(size_t count, size_t size)
 {
